b38d5147045bf1c20d66ed04ea6182caf287fd02
aleksandrakudaeva <alexandrakudaeva@gmail.com>
2022-10-15 17:34:33 +0200

    debug

--- src/tasks/task-3-dashboard/app.py
+++ src/tasks/task-3-dashboard/app.py
@@ -16,90 +16,84 @@ with st.sidebar:
     #text_input = st.text_input('Type the name of the product')
     text_input = st.selectbox('Choose the name of the product', all_items)
 
-    #-------products comparison------
+    # -------products comparison------
 
     st.sidebar.header('CO2 Emission Comparison')
     product_list = df['item'].unique().tolist()
     product_comparison = st.multiselect('Choose the products', product_list)
 
-    #------product comparison end----
+    # ------product comparison end----
 
-try:
+item_row = df[df['item'] == text_input]
 
-    item_row = df[df['item'] == text_input]
-    st.write(item_row)
+co2 = item_row['CO2_per_kg'].iloc[0]
+co2_level = item_row['CO2_level'].iloc[0]
 
-    co2 = item_row['CO2_per_kg'].iloc[0]
-    co2_level = item_row['CO2_level'].iloc[0]
+st.write(co2, co2_level)
 
-    #st.write(co2, co2_level)
+st.markdown("""
+<style>
+div[data-testid="metric-container"] {
+    background-color: rgba(128, 128, 128, 1);
+    border: 1px solid rgba(28, 131, 225, 0.1);
+    padding: 5% 5% 5% 10%;
+    border-radius: 5px;
+    color: rgb(30, 103, 119);
+    overflow-wrap: break-word;
+}
 
-    st.markdown("""
-    <style>
-    div[data-testid="metric-container"] {
-       background-color: rgba(128, 128, 128, 1);
-       border: 1px solid rgba(28, 131, 225, 0.1);
-       padding: 5% 5% 5% 10%;
-       border-radius: 5px;
-       color: rgb(30, 103, 119);
-       overflow-wrap: break-word;
-    }
-    
-    /* breakline for metric text         */
-    div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
-       overflow-wrap: break-word;
-       white-space: break-spaces;
-       color: red;
-    }
-    </style>
-    """
-    , unsafe_allow_html=True)
+/* breakline for metric text         */
+div[data-testid="metric-container"] > label[data-testid="stMetricLabel"] > div {
+    overflow-wrap: break-word;
+    white-space: break-spaces;
+    color: red;
+}
+</style>
+""", unsafe_allow_html=True)
 
-    with open('style.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-    col1, col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
-    with col1:
-        st.metric('CO2 value per kg', co2)
-    with col2:
-        st.metric('CO2 level', co2_level)
+with col1:
+    st.metric('CO2 value per kg', co2)
+with col2:
+    st.metric('CO2 level', co2_level)
 
+# Ploting with Altair
 
-    #if text_input == 'almonds':
-    #    st.image('licensed-image.jpg', width=200)
+if item_row['Agriculture'].iloc[0] == item_row['Food_processing'].iloc[0] == item_row['Transport'].iloc[0] == item_row['Packaging'].iloc[0] == 0:
+    print('zero value')
 
-    # Ploting with Altair
+else:
 
-    if item_row['Agriculture'].iloc[0]==item_row['Food_processing'].iloc[0]==item_row['Transport'].iloc[0]==item_row['Packaging'].iloc[0]==0:
-        print('zero value')
+    # ------Plotting as a Pie-plot-------------
+    pie_plot = pd.melt(item_row, id_vars=['item'], value_vars=[
+                        'Agriculture', 'Food_processing',    'Transport', 'Packaging'], var_name="CO2", value_name="CO2value")
 
-    else:
+    pie_chart = px.pie(pie_plot,
+                        title=f"CO2 of {text_input}",
+                        values="CO2value",
+                        color="CO2",
+                        color_discrete_map={'Agriculture': 'darkgreen',
+                                            'Food_processing': 'lime',
+                                            'Transport': 'yellow',
+                                            'Packaging': 'orange'},
+                        names="CO2"
+                        )
 
-    #------Plotting as a Pie-plot-------------
-        pie_plot = pd.melt(item_row, id_vars=['item'], value_vars=['Agriculture', 'Food_processing',    'Transport', 'Packaging'], var_name="CO2" ,value_name="CO2value")
+    st.plotly_chart(pie_chart)
 
-        pie_chart = px.pie(pie_plot,
-                       title=f"CO2 of {text_input}",
-                       values="CO2value",
-                       color="CO2",
-                       color_discrete_map={ 'Agriculture':'darkgreen',
-                                            'Food_processing':'lime',
-                                            'Transport':'yellow',
-                                            'Packaging':'orange'},
-                       names="CO2"
-                      )
+# ------Product comparison-----
+df = df[df['item'].isin(product_comparison)]
 
-        st.plotly_chart(pie_chart)
+fig2 = px.bar(df, x='CO2_per_kg', y='item', color='CO2_per_kg', log_x=True, title='Carbon Footprint Comparison', labels={
+                'CO2_per_kg': 'Carbon Emission per kg', 'item': 'Products'})
+fig2.update_layout(width=900)
 
-    #------Product comparison-----
-    df = df[df['item'].isin(product_comparison)]
+st.write(fig2)
+# ------Product comparison-----
 
-    fig2 = px.bar(df, x='CO2_per_kg', y='item', color='CO2_per_kg', log_x=True, title='Carbon Footprint Comparison', labels={'CO2_per_kg': 'Carbon Emission per kg', 'item': 'Products'})
-    fig2.update_layout(width=900)
-
-    st.write(fig2)
-    #------Product comparison-----
-
-except:
-    st.write('Sorry, no such product')
+#except:
+#    st.write('Sorry, no such product')
